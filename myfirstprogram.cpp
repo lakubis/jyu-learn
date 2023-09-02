935ce6994858fe074c0f4107f0be7910795c2aac
lakubis <cahyadi.felix@gmail.com>
2023-09-02 15:56:01 +0300

    again

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!" << endl;
-    const int myNum = 15;
-    cout << "I am " << myNum << " years old." << endl;
-    myNum = 10;
-    return 0;
+    int x;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Your number is: " << x;
 }
 
