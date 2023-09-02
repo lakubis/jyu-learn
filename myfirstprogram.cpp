436a1a92e49e64839aa3575a5cdcdfc24760f3a9
lakubis <cahyadi.felix@gmail.com>
2023-09-02 15:50:31 +0300

    another

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     cout << "Hello World!" << endl;
     int myNum = 15;
-    cout << myNum;
+    cout << "I am " << myNum << " years old." << endl;
     return 0;
 }
 
